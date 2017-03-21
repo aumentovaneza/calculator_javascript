3880e5d2a284f05701cf3d71c8b0f6d20a7fe1c0
Vaneza Aumento <vaumento@olinkgroup.com>
2017-03-21 19:30:53 +0800

    -added the interface for the calculator

---
+++ calc.js
@@ -0,0 +1,24 @@
+var val = document.querySelectorAll('number').value;
+
+alert(val);
+    /*
+    document.querySelector("number").onclick = function () {
+        displayInput(val)
+    };
+
+    function displayInput(digit){
+        var display = document.getElementById("display_area");
+
+        if((display.value == "")&& digit =="0"){
+            return;
+        }
+        else{
+            display.value += digit;
+        }
+    }
+
+*/
+
+
+
+
